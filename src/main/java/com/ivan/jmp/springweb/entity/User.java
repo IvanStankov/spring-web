c1956721c85ce0577bb9c8c0a4f2eec89e61bcde
IvanStankov <ivan.stankov1992@gmail.com>
2016-04-22 11:24:38 +0600

    Spring security dependencies

---
+++ src/main/java/com/ivan/jmp/springweb/entity/User.java
@@ -0,0 +1,58 @@
+package com.ivan.jmp.springweb.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by Иван on 22.04.2016.
+ */
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
